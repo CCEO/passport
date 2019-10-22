ef24d060c584af21d3bc1af1e346665e883e6878
Cesar Ureño <cesarurenomartinez@gmail.com>
2019-10-22 17:29:09 -0500

    Create PassportRefreshToken.php

---
+++ src/Bridge/PassportRefreshToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Laravel\Passport\Bridge;
+
+use Illuminate\Database\Eloquent\Model;
+use Hyn\Tenancy\Traits\UsesTenantConnection;
+
+class PassportRefreshToken extends Model
+{
+    use UsesTenantConnection;
+    
+    protected $table = 'oauth_refresh_tokens';
+    
+    protected $fillable = ['id', 'access_token_id', 'revoked', 'expires_at'];
+    
+    public $timestamps = false;
+}
